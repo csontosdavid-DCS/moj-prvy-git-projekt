b77c319fb3a10f29a4399418a0aef7a955f4eb9f
csontosdavid-DCS <csontosdavid@gmail.com>
2026-02-07 14:27:45 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+def pozdrav(meno, jazyk="sk"):
+    if jazyk == "sk":
+        print(f"Ahoj {meno}!")
+    elif jazyk == "en":
+        print(f"Hello {meno}!")
+
+def rozlucka(meno, jazyk="sk"):
+    if jazyk == "sk":
+        print(f"Dovidenia {meno}!")
+    elif jazyk == "en":
+        print(f"Goodbye {meno}!")
+
+pozdrav("David", "sk")
+rozlucka("David", "sk")
