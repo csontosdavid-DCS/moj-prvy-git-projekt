3c3354006dae7c2f0f35ef3f19ff58733ae5714b
David Csontos <macbookpro@MacBook-Pro-uzivatela-David.local>
2026-02-07 13:42:18 +0100

    Pridana podpora en jazyka

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-def pozdrav(meno):
-    print(f"Ahoj {meno}!")
+def pozdrav(meno, jazyk="sk"):
+    if jazyk == "sk":
+        print(f"Ahoj {meno}!")
+    elif jazyk == "en":
+        print(f"Hello {meno}!")
 
-pozdrav("David")
+pozdrav("David", "en")
